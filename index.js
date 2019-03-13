18c6c63f20ed2b6874b0530e478d4689d4f1b143
Ian Davall <iduality5n@gmail.com>
2019-03-13 15:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,4 +38,4 @@ console.log("The original file looks like: ", recipes);
 console.log("But, there is a new object when using the non-destructive method to update is used: ", recipes, " and ", updateObjectWithKeyAndValue(recipes, 'omelette', '3 eggs'));
 destructivelyUpdateObjectWithKeyAndValue(recipes, 'omelette', '3 eggs');
 console.log("Using this destructive method, the original file is updated: ", recipes);
-console.log("Using this non-destructive method to delete, the update 'recipes' file will look like: ", recipes, deleteFromObjectByKey(recipes, 'omelette'));
+console.log("Using this non-destructive method to delete, the updated 'recipes' file will look like: ", recipes, deleteFromObjectByKey(recipes, 'omelette'));
