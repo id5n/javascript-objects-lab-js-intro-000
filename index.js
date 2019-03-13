16a4260a6ecc80116e3e3f1c05c6b7edec5bd3de
Ian Davall <iduality5n@gmail.com>
2019-03-13 14:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let recipes = {};
 
 let updateObjectWithKeyAndValue = function(object, key, value) {
-  return Object.assign({}, object, { [key]: value })
+  return Object.assign({}, object, { [key]: value }) // '{}' is an new empty object that will store the update values without mutating the original object.
 
 };
 
