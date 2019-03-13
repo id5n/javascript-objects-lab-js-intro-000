dc8e21a657f7402b7ddfca77ddbe438636b473b5
Ian Davall <iduality5n@gmail.com>
2019-03-13 14:41:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var recipes = {
-  'eggs' : 3,
-  'cheese' : "2 slices",
-  'paprika' : "1 dash"
-};
 
-var updateObjectWithKeyAndValue = function(object, key, value){
-  var obj = {
-    prop : 1
-  };
-  Object.assign(obj, { prop2 : 2 });
-  return obj;
-};
-updateObjectWithKeyAndValue(obj, 'prop', 'new value');
