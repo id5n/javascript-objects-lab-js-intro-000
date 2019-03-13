f9812fb22ca71c8e56190bdb9334a40c39cb9ee0
Ian Davall <iduality5n@gmail.com>
2019-03-13 14:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ let recipes = {};
 
 let updateObjectWithKeyAndValue = function(object, key, value) {
   return Object.assign({}, object, { [key]: value })
-  // '{}' is an new empty object that will store the update values without mutating the original object.
-  // 'object' is the original object being referenced.
-  // The new 'key' and 'value' will be assigned to the 'object' as a new object.
-
+  /*
+  '{}' is an new empty object that will store the update values without mutating the original object.
+  'object' is the original object being referenced.
+  The new 'key' and 'value' will be assigned to the  original 'object' as a new object, preserving the original state.
+  */
 };
 
 let destructivelyUpdateObjectWithKeyAndValue = function() {
